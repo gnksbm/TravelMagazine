a9e78523cf7c37576453044ae1d73e3495808626
Geonseob Kim <gnksbm@gmail.com>
2024-05-24 21:36:03 +0900

    magazineTableView 구현

--- SeSac_0524_TravelMagazine/ViewController.swift
+++ SeSac_0524_TravelMagazine/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    let tableViewData = MagazineInfo()
+    
+    private lazy var magazineTableView = {
+        let tableView = UITableView()
+        tableView.register(MagazineTableViewCell.self, forCellReuseIdentifier: "MagazineTableViewCell")
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
 
-
+    private func configureUI() {
+        view.addSubview(magazineTableView)
+        magazineTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            magazineTableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            magazineTableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            magazineTableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            magazineTableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+        ])
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        tableViewData.magazine.count
+    }
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineTableViewCell", for: indexPath) as! MagazineTableViewCell
+        cell.configureCell(magazine: tableViewData.magazine[indexPath.row])
+        return cell
+    }
+}
