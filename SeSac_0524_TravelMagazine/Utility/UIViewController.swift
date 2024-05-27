c38fd4749c0d72de2b4e43de520d8bb96680eddf
Geonseob Kim <gnksbm@gmail.com>
2024-05-27 16:30:36 +0900

    검색 바, 네비게이션 바 버튼으로 식당 검색 기능 구현

---
+++ SeSac_0524_TravelMagazine/Utility/UIViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController.swift
+//  SeSac_0524_TravelMagazine
+//
+//  Created by gnksbm on 5/27/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    func showAlert(message: String) {
+        let alertController = UIAlertController(
+            title: message,
+            message: nil,
+            preferredStyle: .alert
+        )
+        let ok = UIAlertAction(title: "확인", style: .default)
+        alertController.addAction(ok)
+        present(alertController, animated: true)
+    }
+}
--- SeSac_0524_TravelMagazine/View/RestaurantListViewController.swift
+++ SeSac_0524_TravelMagazine/View/RestaurantListViewController.swift
@@ -7,26 +7,73 @@
 
 import UIKit
 
-final class RestaurantListViewController: UITableViewController {
-    private let restaurantList = RestaurantList()
+final class RestaurantListViewController: UIViewController {
+    private let list = RestaurantList().restaurantArray
+    private lazy var filteredList = list {
+        didSet {
+            restaurantTableView.reloadData()
+        }
+    }
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var restaurantTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(RestaurantTableViewCell.self)
-        tableView.separatorStyle = .none
+        restaurantTableView.register(RestaurantTableViewCell.self)
+        restaurantTableView.separatorStyle = .none
+        restaurantTableView.dataSource = self
+        searchBar.delegate = self
+        
+        navigationItem.leftBarButtonItem = makeMenuButton()
+    }
+    
+    private func makeMenuButton() -> UIBarButtonItem {
+        let buttonNames = Set(list.map { $0.category })
+        var children = buttonNames.map { title in
+            UIAction(title: "\(title)") { [self] action in
+                filteredList = list.filter { title == $0.category }
+                showAlert(message: "\(filteredList.count)개의 결과가 있습니다")
+            }
+        }
+        let allAction = UIAction(title: "전체보기") { [self] _ in
+            filteredList = list
+        }
+        children.insert(allAction, at: 0)
+        let menu = UIMenu(title: "음식종류", children: children)
+        let barButton = UIBarButtonItem(
+            image: .init(systemName: "line.3.horizontal.decrease.circle"),
+            menu: menu
+        )
+        barButton.tintColor = .black
+        return barButton
     }
 }
-
+// MARK: SearchBar
+extension RestaurantListViewController: UISearchBarDelegate {
+    func searchBar(
+        _ searchBar: UISearchBar,
+        textDidChange searchText: String
+    ) {
+        guard !searchText.isEmpty else {
+            filteredList = list
+            return
+        }
+        filteredList = list.filter { item in
+            item.name.contains(searchText) ||
+            item.category.contains(searchText)
+        }
+    }
+}
 // MARK: TableView
-extension RestaurantListViewController {
-    override func tableView(
+extension RestaurantListViewController: UITableViewDataSource {
+    func tableView(
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     ) -> Int {
-        restaurantList.restaurantArray.count
+        filteredList.count
     }
     
-    override func tableView(
+    func tableView(
         _ tableView: UITableView,
         cellForRowAt indexPath: IndexPath
     ) -> UITableViewCell {
@@ -35,7 +82,7 @@ extension RestaurantListViewController {
             for: indexPath
         )
         cell.configureCell(
-            restaurant: restaurantList.restaurantArray[indexPath.row]
+            restaurant: filteredList[indexPath.row]
         )
         return cell
     }
