52445ac5d3689ea01c84f5ab50dd5c2a029b8b43
Geonseob Kim <gnksbm@gmail.com>
2024-06-02 10:51:16 +0900

    채팅 화면 파일 추가

---
+++ SeSac_0524_TravelMagazine/View/Chat/ChatListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ChatListViewController.swift
+//  SeSac_0524_TravelMagazine
+//
+//  Created by gnksbm on 6/2/24.
+//
+
+import UIKit
+
+final class ChatListViewController: UIViewController {
+    
+}
---
+++ SeSac_0524_TravelMagazine/View/Chat/ChatRoomViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ChatRoomViewController.swift
+//  SeSac_0524_TravelMagazine
+//
+//  Created by gnksbm on 6/2/24.
+//
+
+import UIKit
+
+final class ChatRoomViewController: UIViewController {
+    
+}
